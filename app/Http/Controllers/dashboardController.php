1ffe8a17392c71d39ebab790141060fc63cde834
Daniel Ofori <creativeartghana@gmail.com>
2023-09-28 04:25:46 +0000

    Initial commit

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -10,8 +10,17 @@ class dashboardController extends Controller
     public function showDashboardIndex()
     {
         if (Auth::check()) {
-            return view('Dashboard.index');
-        }else {
+            $user = Auth::user();
+    
+            // Check if the user is authenticated and has completed the signup process
+            if ($user->registration_completed) {
+                return view('Dashboard.index');
+            } else {
+                // Redirect to the signup completion route
+                return redirect()->route('signup.complete');
+            }
+        } else {
+            // User is not authenticated, redirect to the signin page
             return redirect()->route('signin');
         }
     }
